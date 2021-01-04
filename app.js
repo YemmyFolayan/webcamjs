d9c7c68fc8bda6a6d38e74321c887959ba3cf624
YemmyFolayan <foyemc@gmail.com>
2021-01-04 09:24:37 +0100

    Date

--- app.js
+++ app.js
@@ -34,7 +34,10 @@ function take_snapshot() {
   // take snapshot and get image data
   Webcam.snap(function (data_uri) {
     // display results in page
+
     document.getElementById("results").innerHTML =
       "<h2>Here is your image:</h2>" + '<img src="' + data_uri + '"/>';
+
+    console.log(data_uri);
   });
 }
